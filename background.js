2c5a73a043964f19ca5a1dcb3b54ff12c076da5e
Mars Tan <jianxuat@usc.edu>
2020-12-13 23:19:02 +0800

    format

--- background.js
+++ background.js
@@ -1,26 +1,26 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.get('time', function(data) {
-      if (!data.time || data.time < 1607893180000) {
-        chrome.storage.sync.set({time: 1607893180000}, function() {
-          console.log('Time set.');
-        });
-      }
-    });
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.get('time', function (data) {
+    if (!data.time || data.time < 1607893180000) {
+      chrome.storage.sync.set({ time: 1607893180000 }, function () {
+        console.log('Time set.');
+      });
+    }
+  });
 });
 
 let last_alarm = 0;
 const YS = [20, 40, 60, 80, 120, 160];
 
-chrome.alarms.onAlarm.addListener(function( alarm ) {
+chrome.alarms.onAlarm.addListener(function (alarm) {
   if (alarm.name === 'ys') {
-    chrome.storage.sync.get('time', function(data) {
+    chrome.storage.sync.get('time', function (data) {
       const now = Date.now();
       const curr_ys = Math.floor(160 - (data.time - now) / 1000 / 60 / 8);
       console.log(curr_ys);
       if (curr_ys != last_alarm && YS.includes(curr_ys)) {
         new Notification(`现在体力：${curr_ys}`, {
           icon: 'img/ys.png',
-          body: '冲~'
+          body: '冲~',
         });
         last_alarm = curr_ys;
       }
@@ -29,4 +29,4 @@ chrome.alarms.onAlarm.addListener(function( alarm ) {
 });
 
 chrome.alarms.clearAll();
-chrome.alarms.create('ys', {delayInMinutes: 1, periodInMinutes: 1});
+chrome.alarms.create('ys', { delayInMinutes: 1, periodInMinutes: 1 });
--- popup.js
+++ popup.js
@@ -4,54 +4,61 @@ const div = document.getElementById('data');
 const current = document.getElementById('current');
 
 function update(full_time) {
-    chrome.storage.sync.get('time', function(data) {
-        const full_time = data.time;
-        div.innerHTML = '';
-        const now = Date.now();
-        const curr_ys = 160 - (full_time - now) / 1000 / 60 / 8;
-        const second_left = Math.floor((1 - curr_ys % 1) * 8 * 60);
-        const minute = Math.floor(second_left / 60);
-        const second = second_left % 60 < 10 ? `0${Math.floor(second_left % 60)}` : `${Math.floor(second_left % 60)}`;
-        current.innerText = `${Math.floor(curr_ys)} (${minute}:${second})`;
-        YS.forEach(t => {
-            if (curr_ys < t) {
-                const date = new Date(now + (t - curr_ys) * 8 * 60 * 1000);
-                const h = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
-                const m = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
-                const day = date.getDay() === (new Date()).getDay() ? "今天" : "明天";
-                const p = document.createElement('p');
-                p.innerText = `${t}体力: ${day}${h}:${m}`;
-                div.append(p);
-            }
-        });
+  chrome.storage.sync.get('time', function (data) {
+    const full_time = data.time;
+    div.innerHTML = '';
+    const now = Date.now();
+    const curr_ys = 160 - (full_time - now) / 1000 / 60 / 8;
+    const second_left = Math.floor((1 - (curr_ys % 1)) * 8 * 60);
+    const minute = Math.floor(second_left / 60);
+    const second =
+      second_left % 60 < 10
+        ? `0${Math.floor(second_left % 60)}`
+        : `${Math.floor(second_left % 60)}`;
+    current.innerText = `${Math.floor(curr_ys)} (${minute}:${second})`;
+    YS.forEach((t) => {
+      if (curr_ys < t) {
+        const date = new Date(now + (t - curr_ys) * 8 * 60 * 1000);
+        const h =
+          date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
+        const m =
+          date.getMinutes() < 10
+            ? `0${date.getMinutes()}`
+            : `${date.getMinutes()}`;
+        const day = date.getDay() === new Date().getDay() ? '今天' : '明天';
+        const p = document.createElement('p');
+        p.innerText = `${t}体力: ${day}${h}:${m}`;
+        div.append(p);
+      }
     });
+  });
 }
 
 update();
 
-setInterval(function() {
-    update();
+setInterval(function () {
+  update();
 }, 500);
 
-OP.forEach(op => {
-    document.getElementById(`m${op}`).onclick = function(element) {
-        chrome.storage.sync.get('time', function(data) {
-            const curr_ys = 160 - (data.time - Date.now()) / 1000 / 60 / 8;
-            if (curr_ys < op) {
-                return;
-            }
-            const newTime = data.time + op * 8 * 60 * 1000;
-            chrome.storage.sync.set({time: newTime}, function() {
-                update(newTime);
-              });
-        });
-    };
-    document.getElementById(`p${op}`).onclick = function(element) {
-        chrome.storage.sync.get('time', function(data) {
-            const newTime = data.time - op * 8 * 60 * 1000;
-            chrome.storage.sync.set({time: newTime}, function() {
-                update(newTime);
-              });
-        });
-    };
-})
+OP.forEach((op) => {
+  document.getElementById(`m${op}`).onclick = function (element) {
+    chrome.storage.sync.get('time', function (data) {
+      const curr_ys = 160 - (data.time - Date.now()) / 1000 / 60 / 8;
+      if (curr_ys < op) {
+        return;
+      }
+      const newTime = data.time + op * 8 * 60 * 1000;
+      chrome.storage.sync.set({ time: newTime }, function () {
+        update(newTime);
+      });
+    });
+  };
+  document.getElementById(`p${op}`).onclick = function (element) {
+    chrome.storage.sync.get('time', function (data) {
+      const newTime = data.time - op * 8 * 60 * 1000;
+      chrome.storage.sync.set({ time: newTime }, function () {
+        update(newTime);
+      });
+    });
+  };
+});
