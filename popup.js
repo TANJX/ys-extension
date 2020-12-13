8280e75cc1115a457eec0a972cfd769c44877c59
Mars Tan <jianxuat@usc.edu>
2020-12-13 23:16:14 +0800

    show countdown

--- popup.js
+++ popup.js
@@ -1,29 +1,37 @@
-﻿chrome.storage.sync.get('time', function(data) {
-    update(data.time);
-});
-
-const YS = [20, 40, 60, 80, 120, 160];
+﻿const YS = [20, 40, 60, 80, 120, 160];
 const OP = [20, 40, 60];
 const div = document.getElementById('data');
 const current = document.getElementById('current');
 
 function update(full_time) {
-    div.innerHTML = '';
-    const now = Date.now();
-    const curr_ys = 160 - (full_time - now) / 1000 / 60 / 8;
-    current.innerText = `${Math.floor(curr_ys)}`
-    YS.forEach(t => {
-        if (curr_ys < t) {
-            const date = new Date(now + (t - curr_ys) * 8 * 60 * 1000);
-            const h = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
-            const m = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
-            const day = date.getDay() === (new Date()).getDay() ? "今天" : "明天";
-            const p = document.createElement('p');
-            p.innerText = `${t}体力: ${day}${h}:${m}`;
-            div.append(p);
-        }
+    chrome.storage.sync.get('time', function(data) {
+        const full_time = data.time;
+        div.innerHTML = '';
+        const now = Date.now();
+        const curr_ys = 160 - (full_time - now) / 1000 / 60 / 8;
+        const second_left = Math.floor((1 - curr_ys % 1) * 8 * 60);
+        const minute = Math.floor(second_left / 60);
+        const second = second_left % 60 < 10 ? `0${Math.floor(second_left % 60)}` : `${Math.floor(second_left % 60)}`;
+        current.innerText = `${Math.floor(curr_ys)} (${minute}:${second})`;
+        YS.forEach(t => {
+            if (curr_ys < t) {
+                const date = new Date(now + (t - curr_ys) * 8 * 60 * 1000);
+                const h = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
+                const m = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
+                const day = date.getDay() === (new Date()).getDay() ? "今天" : "明天";
+                const p = document.createElement('p');
+                p.innerText = `${t}体力: ${day}${h}:${m}`;
+                div.append(p);
+            }
+        });
     });
 }
+
+update();
+
+setInterval(function() {
+    update();
+}, 500);
 
 OP.forEach(op => {
     document.getElementById(`m${op}`).onclick = function(element) {
