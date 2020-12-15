bf146dd63e179a9c17c34443c1fc16aecc4f44c4
Mars Tan <jianxuat@usc.edu>
2020-12-15 11:42:50 +0800

    bug fixes

--- background.js
+++ background.js
@@ -8,22 +8,29 @@ chrome.runtime.onInstalled.addListener(function () {
   });
 });
 
-let last_alarm = 0;
 const YS = [20, 40, 60, 80, 120, 160];
+
+chrome.storage.local.set({ last_alarm: 0 }, () => {});
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
   if (alarm.name === 'ys') {
     chrome.storage.sync.get('time', function (data) {
-      const now = Date.now();
-      const curr_ys = Math.floor(160 - (data.time - now) / 1000 / 60 / 8);
-      console.log(curr_ys);
-      if (curr_ys != last_alarm && YS.includes(curr_ys)) {
-        new Notification(`现在体力：${curr_ys}`, {
-          icon: 'img/ys.png',
-          body: '冲~',
-        });
-        last_alarm = curr_ys;
-      }
+      const full_time = data.time;
+      chrome.storage.local.get('last_alarm', function (data) {
+        const last_alarm = data.last_alarm;
+        const now = Date.now();
+        const curr_ys = Math.floor(160 - (full_time - now) / 1000 / 60 / 8);
+        console.log('last alarm', last_alarm);
+        console.log('curr_ys', curr_ys);
+        if (curr_ys != last_alarm && YS.includes(curr_ys)) {
+          new Notification(`现在体力：${curr_ys}`, {
+            icon: 'img/ys.png',
+            body: '冲~',
+          });
+          chrome.storage.local.set({ last_alarm: curr_ys }, () => {});
+          console.log('alarm!');
+        }
+      });
     });
   }
 });
